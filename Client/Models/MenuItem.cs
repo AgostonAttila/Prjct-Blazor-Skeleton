912b412992529cf36e98258679e87032ce5a1253
atesz014 <atesz014@gmail.com>
2023-03-30 23:06:14 +0200

    fix sidebars

--- Client/Models/MenuItem.cs
+++ Client/Models/MenuItem.cs
@@ -7,10 +7,7 @@ namespace Client.Models
 	{
 		public string Id { get; set; }
 		public string? Link { get; set; }
-		public string? NormalClass { get; set; }
 		public string? Title { get; set; }
-		public string? MiniClass { get; set; }
-		public string? MiniTitle { get; set; }
 		public List<MenuItem> MenuItemList { get; set; } = new List<MenuItem>();
 	}
 
@@ -20,19 +17,10 @@ namespace Client.Models
 		public string? Id { get; set; }
 		public string? Name { get; set; }
 		public string? Icon { get; set; }
-		public string? Css { get; set; }
 		public string? Type { get; set; }
 		public List<MenuItem> MainMenuItemList { get; set; } = new List<MenuItem>();
 
-	}
-
-
-	public class MainMenuItem
-	{	
-		public string Id { get; set; }	
-		public string Name { get; set; }
-		public List<MenuItem> MenuItemList { get; set; } = new List<MenuItem>();
-	}
+	}	
 }
 
 
