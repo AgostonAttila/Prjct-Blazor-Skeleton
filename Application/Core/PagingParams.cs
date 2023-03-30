760d5e0751f0911b527cd4bbd0d725712ab3a0c0
atesz014 <atesz014@gmail.com>
2023-03-30 23:25:22 +0200

    add Domain starting elements

--- Application/Core/PagingParams.cs
+++ Application/Core/PagingParams.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Application.Core
 {
-	internal class PagingParams
+	public class PagingParams
 	{
+		private const int MaxPageSize = 50;
+		public int PageNumber { get; set; } = 1;
+		private int _pageSize = 10;
+
+		public int PageSize
+		{
+			get => _pageSize;
+			set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+		}
 	}
 }
