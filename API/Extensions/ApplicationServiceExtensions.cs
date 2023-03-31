543ee9b3cd63e5c642a82b9a13b4ce190aaa6df4
atesz014 <atesz014@gmail.com>
2023-03-31 15:36:02 +0200

    add MSSQL,Postgre,sqlite,mysql inmemory to db options

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,46 +20,36 @@ namespace API.Extensions
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
 			});
-			services.AddDbContext<DataContext>(options =>
+
+		
+			services.AddDbContext<DbContext>(options =>
 			{
 				var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-				string connStr;
+				//string connStr = (env == "Development") ? config.GetConnectionString("DefaultConnection") : " ";
 
-				// Depending on if in development or production, use either Heroku-provided
-				// connection string, or development connection string from env var.
-				if (env == "Development")
+				switch (config.GetConnectionString("DBType"))
 				{
-					// Use connection string from file.
-					connStr = config.GetConnectionString("DefaultConnection");
-					//opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-					// opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+					case "mssql":
+						options.UseSqlServer(config.GetConnectionString("ConnectionMSSQL"));
+						break;
+					case "sqlite":
+						options.UseSqlite(config.GetConnectionString("ConnectionSQLite"));
+						break;
+					case "mysql":
+						options.UseMySQL(config.GetConnectionString("ConnectionMySQL"));
+						break;
+					case "postgre":
+						options.UseNpgsql(config.GetConnectionString("ConnectionPostgre"));
+						break;
+					case "inmemory":
+						options.UseInMemoryDatabase(config.GetConnectionString("InMemoryDB"));
+						break;
+					default:
+						options.UseSqlServer(config.GetConnectionString("ConnectionMSSQL"));
+						break;
 				}
-				else
-				{
-
-
-					// Use connection string provided at runtime by Heroku.
-					var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-					// Parse connection URL to connection string for Npgsql
-					connUrl = connUrl.Replace("postgres://", string.Empty);
-					var pgUserPass = connUrl.Split("@")[0];
-					var pgHostPortDb = connUrl.Split("@")[1];
-					var pgHostPort = pgHostPortDb.Split("/")[0];
-					var pgDb = pgHostPortDb.Split("/")[1];
-					var pgUser = pgUserPass.Split(":")[0];
-					var pgPass = pgUserPass.Split(":")[1];
-					var pgHost = pgHostPort.Split(":")[0];
-					var pgPort = pgHostPort.Split(":")[1];
-
-					connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
-				}
-
-				// Whether the connection string came from the local development configuration file
-				// or from the environment variable from Heroku, use it to set up your DbContext.
-				options.UseNpgsql(connStr);
-			});
+			});				
 
 			services.AddCors(opt =>
 			{
---
+++ API/Filters/OnActionExecutingFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Filters
+{
+	public class OnActionExecutingFilter : ActionFilterAttribute
+	{
+		//private readonly UserService _userService;
+
+		//public OnActionExecutingFilter(UserService userService)
+		//{
+		//	_userService = userService;
+		//}
+
+		//public override void OnActionExecuting(ActionExecutingContext context)
+		//{
+		//	if (context.HttpContext.Request.Path.Value.Contains("api"))
+		//	{
+		//		PostRequest request = (PostRequest)context.ActionArguments["postRequest"];
+
+		//		string tryToHack = Shared.Helpers.TextHelper.CheckObjectForHarmfulCodes(ref request);
+
+		//		if (!string.IsNullOrEmpty(tryToHack))
+		//			context.Result = new ContentResult { Content = "INFO_Harmful_Code", StatusCode = 403 };
+		//		//   return HandleResult(_workspaceService.ConvertToIActionResult(new WorkspaceResponse { Error = new Shared.AppError() { ErrorCode = 2, ErrorMessage = "INFO_Warmful_Code", MethodName = MethodInfo.GetCurrentMethod().Name, SourceLayer = "ML" } }));   
+
+		//		if (!context.HttpContext.Request.Path.Value.Contains("api/main") &&
+		//			!context.HttpContext.Request.Path.Value.Contains("login"))
+		//		{
+
+		//			if (!_userService.IsAuthenticatedUser(request?.LoginAuth?.AccessToken))
+		//				context.Result = new ContentResult { Content = "INFO_No_Authenticated", StatusCode = 403 };
+		//			// return HandleResult(_workspaceService.ConvertToIActionResult(new WorkspaceResponse { Error = new Shared.AppError() { ErrorCode = 2, ErrorMessage = "INFO_No_Authenticated", MethodName = MethodInfo.GetCurrentMethod().Name, SourceLayer = "ML" } }));
+
+
+		//		}
+		//	}
+		//}
+	}
+}
