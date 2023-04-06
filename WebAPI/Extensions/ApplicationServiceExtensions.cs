08c8a8a5b6005b6c362bf6319f2015eaa2aeb971
atesz014 <atesz014@gmail.com>
2023-04-06 13:22:59 +0200

    more dbcontext

--- WebAPI/Extensions/ApplicationServiceExtensions.cs
+++ WebAPI/Extensions/ApplicationServiceExtensions.cs
@@ -10,44 +10,76 @@ using System.Configuration;
 
 namespace WebAPI.Extensions
 {
-    public static class ApplicationServiceExtensions
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-            IConfiguration config)
-        {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-           
-            //belerakni többi db-t
-            services.AddDbContext<DataContext>(options =>
-            {               
-                options.UseSqlServer(config.GetConnectionString("sqlConnection"));
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+			IConfiguration config)
+		{
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+			});
 
-            });
+			//belerakni többi db-t
+			services.AddDbContext<DataContext>(options =>
+			{
+				var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+				if (env == "Development")
+				{
+
+					switch (config.GetConnectionString("DBType"))
+					{
+						case "mssql":
+							options.UseSqlServer(config.GetConnectionString("sqlConnection"));
+							break;
+						case "sqlite":
+							options.UseSqlite(config.GetConnectionString("sqliteConnection"));
+							break;
+						case "mysql":
+							options.UseMySQL(config.GetConnectionString("mysqlConnection"));
+							break;
+						case "postgre":
+							options.UseNpgsql(config.GetConnectionString("postgreConnection"));
+							break;
+						case "inmemory":
+							options.UseInMemoryDatabase(config.GetConnectionString("inmemoryConnection"));
+							break;
+						default:
+							options.UseSqlServer(config.GetConnectionString("sqlConnection"));
+							break;
+					}
+				}
+				else
+				{
+					options.UseSqlServer(config.GetConnectionString("sqlConnection"));
+				}
 
 
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithExposedHeaders("WWW-Authenticate", "Pagination")
-                    .WithOrigins(new string[] { "http://localhost:3000", "test" });
-                });
-            });
-            //services.AddMediatR(typeof(List.Handler).Assembly);
-            //services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-            services.AddScoped<IUserAccessor, UserAccessor>();
-            //services.AddScoped<IPhotoAccessor, PhotoAccessor>();
-            services.AddScoped<EmailSender>();    
-            services.AddSignalR();
 
-            return services;
-        }
-    }
+
+			});
+
+
+			services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", policy =>
+				{
+					policy
+					.AllowAnyMethod()
+					.AllowAnyHeader()
+					.AllowCredentials()
+					.WithExposedHeaders("WWW-Authenticate", "Pagination")
+					.WithOrigins(new string[] { "http://localhost:3000", "test" });
+				});
+			});
+			//services.AddMediatR(typeof(List.Handler).Assembly);
+			//services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+			services.AddScoped<IUserAccessor, UserAccessor>();
+			//services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+			services.AddScoped<EmailSender>();
+			services.AddSignalR();
+
+			return services;
+		}
+	}
 }
