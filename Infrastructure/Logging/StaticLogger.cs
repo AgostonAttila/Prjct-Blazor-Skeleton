457dcbd22f2e32fdc3c1c3f6cd9f73104824443f
atesz014 <atesz014@gmail.com>
2023-04-18 15:35:21 +0200

    server - add serilog

---
+++ Infrastructure/Logging/StaticLogger.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Logging
+{
+	public static class StaticLogger
+	{
+		public static void EnsureInitialized()
+		{
+			if (Log.Logger is not Serilog.Core.Logger)
+			{
+				Log.Logger = new LoggerConfiguration()
+					.Enrich.FromLogContext()
+					.WriteTo.Console()
+					.CreateLogger();
+			}
+		}
+	}
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,78 +1,95 @@
-using Microsoft.EntityFrameworkCore;
+
+using Infrastructure.Logging;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.OpenApi.Writers;
-using Persistence;
-using WebAPI.Extensions;
-using static Org.BouncyCastle.Math.EC.ECCurve;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-//services
-builder.Services.AddControllers(opt =>
+StaticLogger.EnsureInitialized();
+Log.Information("Server Booting Up...");
+try
 {
-    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-    opt.Filters.Add(new AuthorizeFilter(policy));
-});
-//.AddFluentValidation(config =>
-//{
-//    config.RegisterValidatorsFromAssemblyContaining<Create>();
-//});
+	var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContextServiceExtensions(builder.Configuration);
+	builder.RegisterSerilog();
 
-//builder.Services.AddDbContext<DataContext>(options =>
-//{
-//	options.UseSqlServer("Server=./;Database=skeleton;User Id=sa3;Password=Titkos!;MultipleActiveResultSets=True;");
-//});
+	//services
+	builder.Services.AddControllers(opt =>
+	{
+		var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+		opt.Filters.Add(new AuthorizeFilter(policy));
+	});
+	//.AddFluentValidation(config =>
+	//{
+	//    config.RegisterValidatorsFromAssemblyContaining<Create>();
+	//});
 
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddIdentityServices(builder.Configuration);
+	//builder.Services.AddDbContextServiceExtensions(builder.Configuration);
+
+	builder.Services.AddDbContext<DataContext>(options =>
+	{
+		options.UseSqlServer("Server=./;Database=skeleton;User Id=sa3;Password=Titkos!;TrustServerCertificate=True;");
+	});
+
+	builder.Services.AddApplicationServices(builder.Configuration);
+	builder.Services.AddIdentityServices(builder.Configuration);
 
 
-builder.Services.AddHttpContextAccessor();
+	builder.Services.AddHttpContextAccessor();
 
-var app = builder.Build();
-app.UseMiddleware<ExceptionMiddleware>();
+	var app = builder.Build();
+	app.UseMiddleware<ExceptionMiddleware>();
 
-//biztonsági szûrések XSS stb
+	//biztonsági szûrések XSS stb
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-	app.UseDeveloperExceptionPage();
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+		app.UseDeveloperExceptionPage();
+	}
+	else
+	{
+		app.Use(async (context, next) =>
+		{
+			context.Response.Headers.Add("Strict-Transport-Security", "max-age=315");
+			await next.Invoke();
+		});
+	}
+
+	app.UseCors("CorsPolicy");
+
+	app.UseDefaultFiles();
+	app.UseStaticFiles();
+	app.UseStaticFiles(new StaticFileOptions()
+	{
+		FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles")),
+		RequestPath = new PathString("/StaticFiles")
+	});
+
+
+
+	app.UseAuthentication();
+	app.UseAuthorization();
+
+	//app.UseHttpsRedirection();
+
+
+	app.MapControllers();
+	app.MapHub<ChatHub>("/chat");
+	app.MapFallbackToController("Index", "Fallback");
+
+
+	await app.MigrateDatabase().RunAsync();
 }
-else
+catch (Exception ex) when (!ex.GetType().Name.Equals("HostAbortedException", StringComparison.Ordinal))
 {
-    app.Use(async (context, next) =>
-    {
-        context.Response.Headers.Add("Strict-Transport-Security", "max-age=315");
-        await next.Invoke();
-    });
+	StaticLogger.EnsureInitialized();
+	Log.Fatal(ex, "Unhandled exception");
 }
-
-app.UseCors("CorsPolicy");
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.UseStaticFiles(new StaticFileOptions()
+finally
 {
-	FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles")),
-	RequestPath = new PathString("/StaticFiles")
-});
-
-
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-//app.UseHttpsRedirection();
-
-
-app.MapControllers();
-app.MapHub<ChatHub>("/chat");
-app.MapFallbackToController("Index", "Fallback");
-
-
-await app.MigrateDatabase().RunAsync();
+	StaticLogger.EnsureInitialized();
+	Log.Information("Server Shutting down...");
+	Log.CloseAndFlush();
+}
